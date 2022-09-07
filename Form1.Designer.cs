4f928b3f5e0a606117e482136c7dccdaa9758e29
ender <ender@NEMSU-PC>
2022-09-08 02:38:11 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace HashCalc
+{
+    partial class frmHashCalc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmHashCalc));
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.btnFile = new System.Windows.Forms.Button();
+            this.btnKiemTra = new System.Windows.Forms.Button();
+            this.txtHash = new System.Windows.Forms.TextBox();
+            this.cbbHash = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnSaoChep = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(53, 12);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.ReadOnly = true;
+            this.txtFile.Size = new System.Drawing.Size(282, 20);
+            this.txtFile.TabIndex = 0;
+            // 
+            // btnFile
+            // 
+            this.btnFile.Location = new System.Drawing.Point(341, 11);
+            this.btnFile.Name = "btnFile";
+            this.btnFile.Size = new System.Drawing.Size(84, 23);
+            this.btnFile.TabIndex = 1;
+            this.btnFile.Text = "Chọn tập tin...";
+            this.btnFile.UseVisualStyleBackColor = true;
+            this.btnFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // btnKiemTra
+            // 
+            this.btnKiemTra.Location = new System.Drawing.Point(96, 36);
+            this.btnKiemTra.Name = "btnKiemTra";
+            this.btnKiemTra.Size = new System.Drawing.Size(329, 23);
+            this.btnKiemTra.TabIndex = 2;
+            this.btnKiemTra.Text = "Kiểm tra";
+            this.btnKiemTra.UseVisualStyleBackColor = true;
+            this.btnKiemTra.Click += new System.EventHandler(this.btnKiemTra_Click);
+            // 
+            // txtHash
+            // 
+            this.txtHash.Location = new System.Drawing.Point(15, 67);
+            this.txtHash.Name = "txtHash";
+            this.txtHash.Size = new System.Drawing.Size(320, 20);
+            this.txtHash.TabIndex = 3;
+            // 
+            // cbbHash
+            // 
+            this.cbbHash.FormattingEnabled = true;
+            this.cbbHash.Items.AddRange(new object[] {
+            "MD5",
+            "SHA1",
+            "SHA256"});
+            this.cbbHash.Location = new System.Drawing.Point(15, 38);
+            this.cbbHash.Name = "cbbHash";
+            this.cbbHash.Size = new System.Drawing.Size(75, 21);
+            this.cbbHash.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Tập tin:";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(15, 94);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(409, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // btnSaoChep
+            // 
+            this.btnSaoChep.Location = new System.Drawing.Point(341, 66);
+            this.btnSaoChep.Name = "btnSaoChep";
+            this.btnSaoChep.Size = new System.Drawing.Size(83, 23);
+            this.btnSaoChep.TabIndex = 9;
+            this.btnSaoChep.Text = "Sao chép";
+            this.btnSaoChep.UseVisualStyleBackColor = true;
+            this.btnSaoChep.Click += new System.EventHandler(this.btnSaoChep_Click);
+            // 
+            // frmHashCalc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 131);
+            this.Controls.Add(this.btnSaoChep);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbbHash);
+            this.Controls.Add(this.txtHash);
+            this.Controls.Add(this.btnKiemTra);
+            this.Controls.Add(this.btnFile);
+            this.Controls.Add(this.txtFile);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmHashCalc";
+            this.Text = "HashCalc";
+            this.Load += new System.EventHandler(this.frmHashCalc_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Button btnFile;
+        private System.Windows.Forms.Button btnKiemTra;
+        private System.Windows.Forms.TextBox txtHash;
+        private System.Windows.Forms.ComboBox cbbHash;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnSaoChep;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace HashCalc
+{
+    public partial class frmHashCalc : Form
+    {
+        public frmHashCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void frmHashCalc_Load(object sender, EventArgs e)
+        {
+            ActiveControl = btnFile;
+            cbbHash.SelectedIndex = 0;
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            txtFile.Clear();
+            txtHash.Clear();
+            OpenFileDialog openFile = new OpenFileDialog();
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                string file = openFile.FileName;
+                try
+                {
+                    txtFile.AppendText(file);
+                    progressBar1.Increment(1);
+                }
+                catch (IOException)
+                {
+                }
+            }
+        }
+
+        public string KiemTraMD5(string file)
+        {
+            using (var md5 = MD5.Create())
+            {
+                var hash = md5.ComputeHash(File.OpenRead(file));
+                return BitConverter.ToString(hash).Replace("-", String.Empty);
+            }
+        }
+        public string KiemTraSHA1(string file)
+        {
+            using (var sha1 = SHA1.Create())
+            {
+                var hash = sha1.ComputeHash(File.OpenRead(file));
+                return BitConverter.ToString(hash).Replace("-", String.Empty);
+            }
+        }
+        public string KiemTraSHA256(string file)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var hash = sha256.ComputeHash(File.OpenRead(file));
+                return BitConverter.ToString(hash).Replace("-", String.Empty);
+            }
+        }
+        private void btnKiemTra_Click(object sender, EventArgs e)
+        {
+            txtHash.Clear();
+
+            if (cbbHash.Text == "MD5")
+            {
+                txtHash.AppendText(KiemTraMD5(txtFile.Text));
+                progressBar1.Increment(100);
+            }
+            else if (cbbHash.Text == "SHA1")
+            {
+                txtHash.AppendText(KiemTraSHA1(txtFile.Text));
+            }
+            else if (cbbHash.Text == "SHA256")
+            {
+                txtHash.AppendText(KiemTraSHA256(txtFile.Text));
+            }
+
+        }
+
+        private void btnSaoChep_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Sao chép thành công!");
+            Clipboard.SetText(txtHash.Text);
+        }
+    }
+}
